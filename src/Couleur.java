98ba4a31b847f20c856b8c752d6e0d1b0fb4e25b
Deschanel Alexandre <alexandre.deschanel@etu.umontpellier.fr>
2023-12-17 10:02:18 +0000

    Update Couleur.java ajout ColorStringE + nouvelle couleur + test()

--- src/Couleur.java
+++ src/Couleur.java
@@ -5,10 +5,10 @@ public enum Couleur {
      * En plus de donner une liste énumérative des couleurs possibles,
      * cette enumération doit permettre à la méthode toString d'une Carte de réaliser un affichage en couleur.
      */
-    ROUGE("\u001B[41m");
+    ROUGE("\u001B[41m"), JAUNE("\u001B[43m"), BLEU("\u001B[44m"), VERT("\u001B[42m"), GRIS("\u001B[47m"), CYAN("\u001B[46m"), VIOLET("\u001B[45m");
 
     private String couleur;
-    private static String texteCouleur = "\u001B[30m";
+    private static String texteNoir = "\u001B[30m";
     private static String annuler = "\u001B[0m";
 
 
@@ -16,7 +16,15 @@ public enum Couleur {
         this.couleur = couleur;
     }
 
-    public String toString(String texte){
-        return this.couleur + texteCouleur + " " + texte + " " + annuler;
+    public String ColorString(String texte){
+        return this.couleur + texteNoir + texte + annuler;
+    }
+    
+    public String ColorStringE(String texte){
+        return "\u001B[51m"+this.couleur + texteNoir + texte + annuler;
+    }
+
+    public static void test(){
+        System.out.print(ROUGE.ColorString(" Je ")+JAUNE.ColorString(" suis ")+BLEU.ColorString(" multi")+VERT.ColorString("color "));
     }
 }
