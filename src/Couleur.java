a0bd434b9a7734123d94a901d75d9a6e499cd892
Deschanel Alexandre <alexandre.deschanel@etu.umontpellier.fr>
2023-12-21 13:13:49 +0000

    Update Couleur.java

--- src/Couleur.java
+++ src/Couleur.java
@@ -5,26 +5,29 @@ public enum Couleur {
      * En plus de donner une liste énumérative des couleurs possibles,
      * cette enumération doit permettre à la méthode toString d'une Carte de réaliser un affichage en couleur.
      */
-    ROUGE("\u001B[41m"), JAUNE("\u001B[43m"), BLEU("\u001B[44m"), VERT("\u001B[42m"), GRIS("\u001B[47m"), CYAN("\u001B[46m"), VIOLET("\u001B[45m");
-
-    private String couleur;
-    private static String texteNoir = "\u001B[30m";
-    private static String annuler = "\u001B[0m";
-
+    ROUGE("\u001B[41m"), JAUNE("\u001B[43m"), VERT("\u001B[42m");
+    /**BLANC("\u001B[47m"), CYAN("\u001B[46m"), VIOLET("\u001B[45m"), BLEU("\u001B[44m")**/
+    private final String couleur;
+    private static final String texteNoir = "\u001B[30m";
+    private static final String annuler = "\u001B[00m";
 
     Couleur(String couleur) {
         this.couleur = couleur;
     }
 
-    public String ColorString(String texte){
-        return this.couleur + texteNoir + texte + annuler;
-    }
-    
-    public String ColorStringE(String texte){
-        return "\u001B[51m"+this.couleur + texteNoir + texte + annuler;
+    public String ColorString(String texte) {
+        String value = this.couleur + texteNoir; // 15
+        int counter = 0;
+        while (counter < texte.length()) {
+            if (texte.charAt(counter) == '\n') {
+                value = value + annuler + '\n' + this.couleur + texteNoir; // 16
+            } else {
+                value = value + texte.charAt(counter);
+            }
+            counter = counter + 1;
+        }
+        return value + annuler;
     }
 
-    public static void test(){
-        System.out.print(ROUGE.ColorString(" Je ")+JAUNE.ColorString(" suis ")+BLEU.ColorString(" multi")+VERT.ColorString("color "));
-    }
+    public static String getAnnuler(){return annuler;}
 }
