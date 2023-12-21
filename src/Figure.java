c9f7d84ebc06532d065ac79a45f098145a9148da
Deschanel Alexandre <alexandre.deschanel@etu.umontpellier.fr>
2023-12-21 13:15:37 +0000

    Update Figure.java

--- src/Figure.java
+++ src/Figure.java
@@ -3,4 +3,14 @@ public enum Figure {
     /**
      * Représente la figure (forme) d'une Carte : ovale , triangle ...
      */
+    CARRE("\u25A0"), TRIANGLE("\u25B2"), LOSANGE("\u25ca");
+    // ECLAIR("\u26A1"), OVALE("O")
+    private String figure;
+    Figure(String figure){
+        this.figure = figure;
+    }
+    public String getFigure(){
+        return figure;
+    }
+
 }
