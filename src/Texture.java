f1d66666a04e59e06ed1ab4ea79ac2fb9f51c808
Deschanel Alexandre <alexandre.deschanel@etu.umontpellier.fr>
2023-12-21 13:18:12 +0000

    Update Texture.java

--- src/Texture.java
+++ src/Texture.java
@@ -3,5 +3,11 @@ public enum Texture {
     /**
      * Représente la texture d'une Carte : pleine , à pois...
      */
+    HACHURE('/'), PLEINE(' '), POIS('°') ;
+    // VAGUE('~'), BANDE('|'), LIGNE('-')
+    private char forme;
+    Texture(char forme){ this.forme = forme;}
+
+    public char getForme(){return this.forme;}
 
 }
