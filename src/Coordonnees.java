22e88e0e65d6d4cc1ee56a9862c61cb555bedf03
Deschanel Alexandre <alexandre.deschanel@etu.umontpellier.fr>
2024-01-07 09:37:59 +0000

    Update Coordonnees.java

--- src/Coordonnees.java
+++ src/Coordonnees.java
@@ -3,7 +3,15 @@ public class Coordonnees {
     private int colonne;
 
 
+/**
+ * La classe Coordonnees représente les coordonnées (i,j) d'une Carte sur la Table
+ * ou i représenta la ligne et j la colonne
+ * Cette classe est utilisée uniquement pour intéragir avec l'utilisateur
+ *  */
+
+
     /**
+     * Pre-requis : x,y>=0
      * Action : Construit des Coordonnées ayant x comme numéro de ligne et y comme numéro de colonne
      */
     public Coordonnees(int x, int y) {
@@ -12,6 +20,8 @@ public class Coordonnees {
     }
 
     /**
+     * Pre-requis : input est sous la forme  suivante : int,int
+     * Action : Construit des Coordonnées ayant x comme numéro de ligne et y comme numéro de colonne
      */
     public Coordonnees(String input) {
         String[] splited = input.split(",");
@@ -35,6 +45,7 @@ public class Coordonnees {
     }
 
     /**
+     * Pre-requis : aucun
      * Action : Retourne vrai si la variable input est dans un format valide à savoir int,int
      * Aide : On peut utiliser Ut.estNombre pour vérifier qu'une chaîne de caractères est bien un nombre.
      */
